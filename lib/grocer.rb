b5d1f651b47b65c608ad52b4d580103208bfa374
Ryan Werner <rmwerner@umich.edu>
2020-07-11 15:47:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,6 @@
 def find_item_by_name_in_collection(name, collection)
-  collection.detect do |name|
-    if name
+  collection.each do |item|
+    if item == name 
       return collection[name.index]
     else
       return nil
