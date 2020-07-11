72f80d0d49f398cf3d691e54bb59bed0fbf8dc0d
Ryan Werner <rmwerner@umich.edu>
2020-07-11 16:06:32 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,8 +2,7 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
-    binding.pry
-    if item.include? name
+    if item[:item] == name
       return item
     end
   end
